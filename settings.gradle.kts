2fc22d11298d68f08dd796545098ecc9b311bef8
Przemek Malirz <p.malirz@gmail.com>
2023-04-27 22:09:08 +0200

    Merge pull request #2
    * Fixes after manipulating host and container names
    
    * Remove env from the mongo Dockerfile

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,11 @@
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.8.10"
-        kotlin("plugin.spring") version "1.8.10"
-        kotlin("plugin.noarg") version "1.8.10"
-        kotlin("plugin.jpa") version "1.8.10"
-        id("org.springframework.boot") version "3.0.4"
+        kotlin("jvm") version "1.8.20"
+        kotlin("js") version "1.8.20"
+        kotlin("plugin.spring") version "1.8.20"
+        kotlin("plugin.noarg") version "1.8.20"
+        kotlin("plugin.jpa") version "1.8.20"
+        id("org.springframework.boot") version "3.0.6"
         id("io.spring.dependency-management") version "1.1.0"
     }
 }
