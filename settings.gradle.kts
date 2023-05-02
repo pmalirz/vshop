3bab99bedd40efecc3d31203e61508588c685fcb
pmalirz <p.malirz@gmail.com>
2023-05-02 18:59:45 +0200

    Add com.gradle.enterprise.gradle.plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,20 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version("3.13")
+}
 
 rootProject.name = "vshop"
 
 include("vshop-docker", "vshop-webapp")
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
